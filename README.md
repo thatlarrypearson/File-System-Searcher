62590bbba5877a4bc1f600fd1ae11b7b19952d24
runarut <runarut@users.noreply.github.com>
2020-02-01 09:37:12 -0600

    completed features, improved documentation

--- README.md
+++ README.md
@@ -8,42 +8,82 @@ FileSystemSearcher \[options\] \[base_path1 \[base_path2\] ... \]
 
 ### \[options\]
 * ```--volume=name```
+
   Output the user provided ```volume``` name with all output records.  Default is None.  Used to help users associate external USB drives with records.
-* ```--output-file-format=off```
-  Output file format or 'off' can be one of ```text```,  ```CSV``` or ```JSON```.
+
+* ```--output_file=name```
+
+  Output file name.  Defaults to standard output (stdout).
+
+* ```--output_format=format```
+
+  Output format or can be one of ```text```,  ```CSV``` or ```JSON```.
+
   * ```text```
+
     Simplistic report format
+
   * ```CSV```
+
     Comma seperated values that can be imported by Microsoft Excel.  See the Python [csv](https://docs.python.org/3/library/csv.html) library.
+
   * ```JSON```
+
     The output is put into a ```list()``` of records composed of dictionaries - ```dict()```.  The output is then converted into JSON format.
+
+### Base Paths
 * ```base_path```
   The relative or absolute path to the directory where the file search should begin.  When not present, the ```base_path``` defaults to the current working directory.
 
 ## Output Fields
 
 * hostname
+
   The computer's name on the network.
+
 * volume
+
   User provided volume name associated with all files in the output set.
+
 * file_name
+
   The file name indipeneent of where it sits within the file system.
+
 * relative_path
+
   The directory path from the base path to the directory the file sits in.
+
 * base_path
+
   The "base path" is the full path to the directory where the search for files started.
+
 * size
+
   File size in bytes.
+
 * dropbox_hash
+
   A hash of hashes made using the same method that Dropbox uses in their public API's.
+
 * created
+
   File creation timestamp or None if not available.
+
 * modified
+
   File last modified timestamp or None if not available.
+
 * suffix
+
   File suffix.  E.g. file name '```picture.jpg```' suffix is '```jpg```'.
+
 * mime_type
-  MIME type as determined by [Python ```mimetypes```](https://docs.python.org/3/library/mimetypes.html)
+
+  MIME type is determined by [Python ```mimetypes```](https://docs.python.org/3/library/mimetypes.html)
+
+* MIME encoding
+
+  Encoding is determined by [Python ```mimetypes```](https://docs.python.org/3/library/mimetypes.html)
 
 ## File Formats
 
@@ -51,11 +91,14 @@ FileSystemSearcher \[options\] \[base_path1 \[base_path2\] ... \]
 
 * JSON
 
+* Text
+
 ## Required Python Versions
 
 The software has been tested on:
 
 * Windows 10 on 64 Bit X86
+
   * Python 3.8
   * Python 3.7
   * Anaconda Python 3.8
@@ -63,18 +106,18 @@ The software has been tested on:
   * Anaconda Python 3.6
 
 * Linux Mint on 32 and 64 Bit X86
+
   * Python 3.8
   * Python 3.7
   * Python 3.6
 
 * Raspbian on 64 Bit ARM7
+
   * Python 3.8
   * Python 3.7
   * Python 3.6
 
 ## Requierd Python Libraries
 
-The following Python Libraries are required for installation.
-
-* [```python-magic```](https://github.com/ahupp/python-magic)
+No required Python libraries.  Yet.
 
--- src/FileSystemSearcher.py
+++ src/FileSystemSearcher.py
@@ -1,8 +1,11 @@
 # FileSystemSearcher.py
 import os
+import sys
 import pytz
 import mimetypes
 import socket
+import json
+import csv
 from pathlib import Path, WindowsPath, PurePath
 from platform import uname
 from argparse import ArgumentParser
@@ -29,14 +32,103 @@ def dropbox_hash(path):
 def convert_datetime_to_utc(dt):
     return pytz.utc.localize(dt)
 
+class Publish():
+    def __init__(self, output_format, output_fd):
+        self.fd = output_fd
+        self.record_count = 0
+        self.header = None
+        self.body = None
+        self.footer = None
+
+        if output_format == 'txt':
+            self.header = self.txt_header
+            self.body = self.txt_body
+            self.footer = self.txt_footer
+        elif output_format == 'json':
+            self.header = self.json_header
+            self.body = self.json_body
+            self.footer = self.json_footer
+        elif output_format == 'csv':
+            self.header = self.csv_header
+            self.body = self.csv_body
+            self.footer = self.csv_footer
+        else:
+            raise ValueError("Not a valid output format: %s" % (output_format, ))
+    
+    def txt_header(self, record):
+        first_key = True
+        s = ''
+        k_list = list(record)
+        k_list.sort()
+        for k in k_list:
+            if not first_key:
+                s = s + "\t"
+            else:
+                first_key = False
+            s = s + k
+        print(s, file=self.fd)
+        self.txt_body(record)
+    
+    def txt_body(self, record):
+        first_key = True
+        s = ''
+        k_list = list(record)
+        k_list.sort()
+        for k in k_list:
+            if not first_key:
+                s = s + "\t"
+            else:
+                first_key = False
+            s = s + str(record[k])
+        print(s, file=self.fd)
+        self.record_count = self.record_count + 1
+
+    def txt_footer(self):
+        print("Records: %d" % (self.record_count, ), file=self.fd)
+
+    def json_header(self, record):
+        print('[', file=self.fd)
+        self.json_body(record)
+
+    def json_body(self, record):
+        print(json.dumps(record, sort_keys=True) + ',', file=self.fd)
+        self.record_count = self.record_count + 1
+
+    def json_footer(self):
+        print(']', file=self.fd)
+
+    def csv_header(self, record):
+        k_list = list(record)
+        k_list.sort()
+        self.csvwriter = csv.writer(self.fd)
+        self.csvwriter.writerow(k_list)
+        self.csv_body(record)
+
+    def csv_body(self, record):
+        k_list = list(record)
+        k_list.sort()
+        r_list = []
+        for k in k_list:
+            r_list.append(record[k])
+        self.csvwriter.writerow(r_list)
+
+    def csv_footer(self):
+        pass
+
+
 # https://docs.python.org/3/library/pathlib.html
 # https://docs.python.org/3/library/mimetypes.html
 # https://docs.python.org/3/library/csv.html
+# https://sahandsaba.com/interview-question-iterator-of-iterators-and-cantor-set-theory.html
 
 
 class Crawler():
-    def __init__(self):
+    def __init__(self, volume=None, verbose=False):
         self.current_working_directory = Path.cwd()
+        self.volume = volume
+        self.verbose = verbose
+        self.hostname = socket.gethostname()
+        self.base_path = None
 
     def base_to_absolute_path(self, base_path):
         if base_path is None:
@@ -53,42 +145,107 @@ class Crawler():
                 
         # remove '/../' and parent dir with resolve()
         return absolute_base_path.resolve()
-    
+
     def file_filter(self, relative_path):
         return False
 
+    def path_crawler(self, base_path):
+        self.base_path = self.base_to_absolute_path(base_path)
+        return self
+
+    def __iter__(self):
+        self.path_iterator = self.base_path.glob('**/*')
+        self.path_iterator.__init__()
+        return self
+
+    def __next__(self):
+        p = self.path_iterator.__next__()   
+        while not p.is_file() or self.file_filter(p.name):
+            # for things that are not files:
+            # verbose print the type of thing it is (str(type(p).__name__)) followed by the path
+            p = self.path_iterator.__next__()
+
+        record = {
+            'hostname': self.hostname,
+            'volume': self.volume,
+            'file_name': p.name,
+            'relative_path': str(p.relative_to(self.base_path)),
+            'full_path': str(self.base_path / p),
+            'size': int(p.stat().st_size),
+            'dropbox_hash': dropbox_hash(p),
+            'created': (convert_datetime_to_utc(datetime.fromtimestamp(p.stat().st_ctime))).isoformat(),
+            'modified': (convert_datetime_to_utc(datetime.fromtimestamp(p.stat().st_mtime))).isoformat(),
+            'suffix': p.suffix,
+            'mime_type': None,
+            'mime_encoding': None,
+        }
+
+        record['mime_type'], record['mime_encoding'] = mimetypes.guess_type(p, strict=False)
+
+        return record
+
     def file_system_search(self, base_path):
-        path = self.base_to_absolute_path(base_path)
-        for p in path.glob('**/*'):
-            if not p.is_file():
-                # for things that are not files:
-                # verbose print the type of thing it is (str(type(p).__name__)) followed by the path
-                continue
-
-            if self.file_filter(p.name):
-                continue
-
-            record = {
-                'hostname': socket.gethostname(),
-                'file_name': p.name,
-                'relative_path': p.relative_to(path),
-                'full_path': path / p,
-                'size': int(p.stat().st_size),
-                'dropbox_hash': dropbox_hash(p),
-                'created': convert_datetime_to_utc(datetime.fromtimestamp(p.stat().st_ctime)),
-                'modified': convert_datetime_to_utc(datetime.fromtimestamp(p.stat().st_mtime)),
-                'suffix': p.suffix,
-                'mime_type': None,
-                'mime_encoding': None,
-            }
-            record['mime_type'], record['mime_encoding'] = mimetypes.guess_type(p, strict=False)
+        for record in self.path_crawler(base_path):
             self.output(record)
 
-
     def output(self, record):
-        print('\n', record, '\n')
+        print(record)
 
 
-crawler = Crawler()
+# https://docs.python.org/3/howto/argparse.html
+# https://docs.python.org/3/library/argparse.html#module-argparse
+# https://docs.python.org/3/library/argparse.html
 
-crawler.file_system_search('..')
+
+def main():
+    parser = ArgumentParser(
+            description="File System Searcher - Search for files and output records with useful info."
+        )
+    parser.add_argument(
+            "base_paths",
+            nargs='*',
+            metavar="base_path",
+            default=[".", ],
+            help="Relative or absolute directory path where the file search begins. Default: current working directory."
+        )
+    parser.add_argument("-v", "--verbose", help="Enable verbose mode", action="store_true")
+    parser.add_argument("--output_file", help="Output file name.", default=sys.stdout)
+    parser.add_argument(
+            "--volume", 
+            help="""Output the user provided volume name with all output records.
+            Used to help users associate external USB drives with records.""",
+            default=None
+        )
+    parser.add_argument(
+            "--output_format",
+            help="Output format",
+            choices=["txt", "csv", "json", ],
+            default="txt"
+        )
+    args = vars(parser.parse_args())
+
+    if isinstance(args['output_file'], str):
+        if args['output_format'] == 'csv':
+            output_fd = open(args['output_file'], mode='w', newline='')
+        else:
+            output_fd = open(args['output_file'], mode='w')
+    else:
+        output_fd = args['output_file']
+
+    crawler = Crawler(volume=args['volume'], verbose=args['verbose'])
+    
+    publish = Publish(args['output_format'], output_fd)
+
+    first_time = True
+    for base_path in args['base_paths']:
+        for record in crawler.path_crawler(base_path):
+            if first_time:
+                first_time = False
+                publish.header(record)
+            else:
+                publish.body(record)
+        publish.footer()
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
